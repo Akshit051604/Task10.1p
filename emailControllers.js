2209ff8dfbca04e25f57464a00b6fc9fe95cb4bc
Akshit051604 <akshitbaren0516@gmail.com>
2024-11-13 18:37:31 +0530

    Update emailControllers.js

--- emailControllers.js
+++ emailControllers.js
@@ -1,36 +1,56 @@
-const expressAsyncHandler = require("express-async-handler");
-const dotenv = require("dotenv");
-const nodemailer = require("nodemailer");
-dotenv.config();
+const express = require('express')
+const bodyparser = require('body-parser')
+const https = require('https')
+const e = require('express')
+const app = express()
 
-let transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: process.env.SMTP_MAIL, // generated ethereal user
-    pass: process.env.SMTP_PASSWORD, // generated ethereal password
-  },
-});
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.static('public'))
 
-const sendEmail = expressAsyncHandler(async (req, res) => {
-  const { email, subject, message } = req.body;
-  console.log(email, subject, message);
+app.listen(8080, function(request, response){
+console.log('port is running on 8080')
+})
 
-  var mailOptions = {
-    from: process.env.SMTP_MAIL,
-    to: email,
-    subject: subject,
-    text: message,
-  };
+app.get('/', function(request, response)
+{
+    response.sendFile(__dirname + '/index.html')
+})
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent successfully!");
+app.post('/', function(request,response)
+{
+    const fname = request.body.firstname;
+    const lname = request.body.lastname;
+    const email = request.body.gmail;
+    console.log(fname, lname, email);
+
+    const apik = "0bd0dd8e22afe20e23971b89885f9694-us22";
+    const list_id = "58ec24c56e";
+    const url = "https://us22.api.mailchimp.com/3.0/lists/58ec24c56e";
+
+    const options = {
+        method:"POST",
+        auth:"ak:0bd0dd8e22afe20e23971b89885f9694-us22"
     }
-  });
-});
+    const data= {
+        members: [{email_addres:email,
+        status:"subscribed",
+        merge_field: {
+            FNAME:fname,
+            LNAME:lname
+        }
+    }]
+    }
+    const jsonData = JSON.stringify(data)    
 
-module.exports = { sendEmail };
+    const req= https.request(url, options, function(response)
+    {
+        response.on("data", function(data)
+    {
+        console.log(jsonData.parse(data))
+    })
+    })
+
+    req.write(jsonData)
+    req.end()
+    console.log(fname, lname, email)
+})
