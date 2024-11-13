524573745fd710e44042d4259a6473b20b3fec0d
Akshit051604 <akshitbaren0516@gmail.com>
2024-11-13 18:38:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const bodyparser = require('body-parser')
+const https = require('https')
+const e = require('express')
+const app = express()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.static('public'))
 
-var app = express();
+app.listen(8080, function(request, response){
+console.log('port is running on 8080')
+})
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/', function(request, response)
+{
+    response.sendFile(__dirname + '/index.html')
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.post('/', function(request,response)
+{
+    const fname = request.body.firstname;
+    const lname = request.body.lastname;
+    const email = request.body.gmail;
+    console.log(fname, lname, email);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    const apik = "0bd0dd8e22afe20e23971b89885f9694-us22";
+    const list_id = "58ec24c56e";
+    const url = "https://us22.api.mailchimp.com/3.0/lists/58ec24c56e";
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    const options = {
+        method:"POST",
+        auth:"ak:0bd0dd8e22afe20e23971b89885f9694-us22"
+    }
+    const data= {
+        members: [{email_addres:email,
+        status:"subscribed",
+        merge_field: {
+            FNAME:fname,
+            LNAME:lname
+        }
+    }]
+    }
+    const jsonData = JSON.stringify(data)    
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    const req= https.request(url, options, function(response)
+    {
+        response.on("data", function(data)
+    {
+        console.log(jsonData.parse(data))
+    })
+    })
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+    req.write(jsonData)
+    req.end()
+    console.log(fname, lname, email)
+})
